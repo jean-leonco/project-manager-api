69fe29cfdc8859a75df0120d0806a1bfe40aed83
Jean Leonço <jeang.leonco@gmail.com>
2019-10-07 18:13:22 -0300

    chore: project crud

--- start/routes.js
+++ start/routes.js
@@ -13,4 +13,5 @@ Route.group(() => {
 
 Route.group(() => {
   Route.post('invites', 'InviteController.store')
+  Route.resource('projects', 'ProjectController').apiOnly()
 }).middleware(['auth', 'team'])
